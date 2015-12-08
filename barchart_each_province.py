837e513e82cfda9cbdef8005beaa4668dfdfd3c6
sea199676 <it58070117@office365.it.kmitl.ac.th>
2015-12-08 15:27:28 +0700

    make a barchart every year

---
+++ barchart_each_province.py
@@ -0,0 +1,21 @@
+import csv
+import plotly.plotly as py
+import plotly.graph_objs as go
+py.sign_in('sea199676', 'vrmlh14j43')
+years = int(input())
+name_file = "data%d.txt"%years
+file=open(name_file, 'rt')
+data=csv.reader(file)
+table=[row for row in data]
+print(table[:])
+province = [name for [name, outcome, income, debt] in table]
+outcome_money = [int(outcome) for [name, outcome, income, debt] in table]
+income_money = [int(income) for [name, outcome, income, debt] in table]
+debt_money = [int(debt) for [name, outcome, income, debt] in table]
+outcome = go.Bar(x=province, y=outcome_money, name="Outcome")
+income = go.Bar(x=province, y=income_money, name="Income")
+debt = go.Bar(x=province, y=debt_money, name="Debt")
+data = [outcome, income, debt]
+layout = go.Layout(barmode='group')
+fig = go.Figure(data=data, layout=layout)
+plot_url = py.plot(fig, filename='grouped-bar')

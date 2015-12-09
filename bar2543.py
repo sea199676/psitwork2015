12ff5ff274f40a6c8aef250a0b6dd5f3ec612e7d
sea199676 <it58070117@office365.it.kmitl.ac.th>
2015-12-09 10:36:14 +0700

    Revert "Revert "example of bargraph in year 2543""
    This reverts commit 9e31447dafc62b5b4ea1186063ec2d5b7e5947ee.

--- bar2543.py
+++ bar2543.py
@@ -2,7 +2,7 @@ import csv
 import plotly.plotly as py
 import plotly.graph_objs as go
 py.sign_in('sea199676', 'vrmlh14j43')
-years = 2556
+years = 2543
 name_file = "data%d.txt"%years
 file=open(name_file, 'rt')
 data=csv.reader(file)
@@ -17,4 +17,4 @@ debt = go.Bar(x=province, y=debt_money, name="Debt")
 data = [outcome, income, debt]
 layout = go.Layout(barmode='group')
 fig = go.Figure(data=data, layout=layout)
-plot_url = py.plot(fig, filename='barchart2556')
+plot_url = py.plot(fig, filename='grouped-bar')
